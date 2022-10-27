fe558dcb1bf94222f45078e168ac992e1f673ee0
leehtys <105432859+leehtys@users.noreply.github.com>
2022-10-27 09:34:37 -0300

    Update calcula-imc.js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Leticia Nutrição";
 
 var pacientes = document.querySelectorAll(".paciente");
 
